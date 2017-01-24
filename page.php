953b308f0fac0a606185e2fd78d000ee477c03b1
johnwatkins0 <johnwatkins0@gmail.com>
2017-01-24 13:46:07 -0500

    Fix template inclusion for page

--- page.php
+++ page.php
@@ -4,4 +4,4 @@ if ( has_post_thumbnail() ) {
     return Colby_College\Wp_Components\Wp_Templates::load( 'page-with-header-image.php' );
 }
 
-return Colby_College\Wp_Components\Wp_Templates::load( 'index.php' );
+return Colby_College\Wp_Components\Wp_Templates::load( 'page.php' );
