37b87ebf1b33371ef86da1ee14ac63aea0cf522b
johnwatkins0 <johnwatkins0@gmail.com>
2017-01-27 07:47:11 -0500

    Add banner image captions

--- page.php
+++ page.php
@@ -1,6 +1,15 @@
 <?php
 
 if ( has_post_thumbnail() ) {
+    add_filter( 'page_has_thumbnail_article_footer', function( $content ) {
+        global $post;
+        ob_start();
+        echo '<div class=page-has-thumbnail__footer-caption>Banner image: ';
+        the_post_thumbnail_caption( $post->ID );
+        echo '</div>';
+        return ob_get_clean();
+    } );
+
     return Colby_College\Wp_Components\Wp_Templates::load( 'page-with-header-image.php' );
 }
 
