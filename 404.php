3b9f4486625f808bed44b1ec2cd08e08e7cb3764
johnwatkins0 <johnwatkins0@gmail.com>
2017-01-29 12:31:08 -0500

    Add 404 file

--- 404.php
+++ 404.php
@@ -1,1 +1,20 @@
-hello
+<?php
+if ( ! have_posts() ) {
+    return include( locate_template( '404.php' ) );
+}
+
+get_header(); ?>
+
+<main id=main class=main--index>
+
+<article <?php post_class(); ?>>
+    <header>
+        <h1>No posts found.</h1>
+    </header>
+
+</article>
+
+</main>
+
+<?php
+get_footer();
