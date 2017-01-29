bfb441b03f9643395a24e77202344cfff315fc25
johnwatkins0 <johnwatkins0@gmail.com>
2017-01-29 12:24:53 -0500

    Create archive and single post templates

--- functions.php
+++ functions.php
@@ -5,3 +5,9 @@ global $lunder_institute;
 require_once( 'vendor/autoload.php' );
 
 $lunder_institute = new Colby_College\Wp_Components\Colby_Wp_Theme();
+
+add_action( 'pre_get_posts', function( $query ) {
+	if ( is_home() && $query->is_main_query() ) {
+		$query->set( 'category__not_in', [ get_cat_ID( 'Media Kit' ) ] );
+	}
+} );
---
+++ home.php
@@ -0,0 +1,3 @@
+<?php
+
+return Colby_College\Wp_Components\Wp_Templates::load( 'home.php' );
