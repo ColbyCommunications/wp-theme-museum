56dc887ddf26288eb709ba28d5d82998ebf34175
johnwatkins0 <johnwatkins0@gmail.com>
2017-01-29 21:17:44 -0500

    Remove unwanted scripts and styles

--- page.php
+++ page.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Page.php
+ *
+ * Template for a single post of the "page" post type.
+ *
+ * @package lunder-institute
+ */
 
 if ( ! has_post_thumbnail() ) {
 	return Colby_College\Wp_Components\Wp_Templates::load( 'page.php' );
@@ -6,21 +13,22 @@ if ( ! has_post_thumbnail() ) {
 
 /** Add banner image caption to article footer. */
 add_filter( 'page_has_thumbnail_article_footer', function( $content ) {
-    global $post;
+	global $post;
 
-    $caption = get_the_post_thumbnail_caption( $post->ID );
+	$caption = get_the_post_thumbnail_caption( $post->ID );
 
-    if ( ! $caption ) {
-        return '';
-    }
+	if ( ! $caption ) {
+		return '';
+	}
 
-    ob_start();
-    echo "
-    <div class=page-has-thumbnail__footer-caption>
-        Banner image: $caption
-    </div>
-    ";
-    return ob_get_clean();
+	ob_start();
+	?>
+	<div class=page-has-thumbnail__footer-caption>
+		Banner image: <?php echo wp_kses_post( $caption ); ?>
+	</div>
+
+	<?php
+	return ob_get_clean();
 } );
 
 Colby_College\Wp_Components\Wp_Templates::load( 'page-with-header-image.php' );
